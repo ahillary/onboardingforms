8d5ffb45b7be9fdbc5d0c978dcc864ff2618bf9b
Hillary Jenning <hello.hillary7@gmail.com>
2022-07-19 16:33:09 -0700

    feat(client): user actions
    get all users action

--- client/src/app/store/user/userAction.js
+++ client/src/app/store/user/userAction.js
@@ -1,24 +1,23 @@
 // action types
-export const GET_USER = 'GET_USER';
 export const CREATE_USER = 'CREATE_USER';
+export const GET_USER = 'GET_USER';
+export const GET_USERS = 'GET_USERS';
 export const UPDATE_USER = 'UPDATE_USER';
-export const INVALID_EMAIL = 'INVALID_EMAIL';
 
 //action creators
-export const fetchUser = (findThisUser) => ({
-  type: GET_USER,
-  user: findThisUser,
-});
 export const addUser = (addThisUser) => ({
   type: CREATE_USER,
   user: addThisUser,
 });
+export const fetchUser = (findThisUser) => ({
+  type: GET_USER,
+  user: findThisUser,
+});
+export const fetchUsers = (AllUsers) => ({
+  type: GET_USERS,
+  user: AllUsers,
+});
 export const putUser = (updateThisUser) => ({
   type: UPDATE_USER,
   user: updateThisUser,
-});
-
-export const invalidEmail = (email, error) => ({
-  type: INVALID_EMAIL,
-  error: `${error} error with ${email}`,
 });
