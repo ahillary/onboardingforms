e6b5a74ccdd9843c06f772ca3d736c1ae6eab220
Hillary Jenning <hello.hillary7@gmail.com>
2022-07-20 17:34:04 -0700

    refactor(client): update user reducer in store

--- client/src/app/store/user/user.js
+++ client/src/app/store/user/user.js
@@ -11,15 +11,26 @@ export const currentUser = (username) => async (dispatch) => {
     console.log('What have you done?!', error);
   }
 };
+
+// while it would be ideal to use the user.id to find a user, in the edge case that more than one individual is creating an account simutaneously it would mess up the process with this function as written:
+// export const findThem = () => async (dispatch) => {
+//   try {
+//     const { data } = await axios.get(`${apiUrl}/api/user`);
+//     dispatch(fetchUser(data));
+//   } catch (error) {
+//     console.log('What have you done?!', error);
+//   }
+// };
+
 // initial state
 const initialState = [];
 
 /* REDUCER */
-export default function userReducer(users = initialState, action) {
+export default function userReducer(user = initialState, action) {
   switch (action.type) {
     case GET_USER:
       return action.user;
     default:
-      return users;
+      return user;
   }
 }
