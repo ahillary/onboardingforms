47fc598b0d62531f61a8646d54212d96fdc623b0
Hillary Jenning <hello.hillary7@gmail.com>
2022-07-26 16:10:06 -0700

    refactor(client): find user w/email, not username

--- client/src/app/store/user/user.js
+++ client/src/app/store/user/user.js
@@ -3,24 +3,15 @@ import { GET_USER, fetchUser } from './userAction';
 import { apiUrl } from '../../../properties';
 
 //thunk creator
-export const currentUser = (username) => async (dispatch) => {
+
+export const currentUser = (email) => async (dispatch) => {
   try {
-    const { data } = await axios.get(`${apiUrl}/api/users/${username}`);
+    const { data } = await axios.get(`${apiUrl}/api/user/${email}`);
     dispatch(fetchUser(data));
   } catch (error) {
     console.log('What have you done?!', error);
   }
 };
-
-// while it would be ideal to use the user.id to find a user, in the edge case that more than one individual is creating an account simutaneously it would mess up the process with this function as written:
-// export const findThem = () => async (dispatch) => {
-//   try {
-//     const { data } = await axios.get(`${apiUrl}/api/user`);
-//     dispatch(fetchUser(data));
-//   } catch (error) {
-//     console.log('What have you done?!', error);
-//   }
-// };
 
 // initial state
 const initialState = [];
