8c6a2439d941e965eb6681ced0998320b68b75b7
Hillary Jenning <hello.hillary7@gmail.com>
2022-07-30 14:28:54 -0700

    refactor(server): userRouter add post, update put
    add a user (router.post) relocation to userRouter
    
    update user with email instead of username
    
    console.logs for testing

--- server/api/userRouter.js
+++ server/api/userRouter.js
@@ -28,7 +28,7 @@ const { connect } = require('../db');
 // });
 
 // for returning users that left and will finish
-// GET /user/:email - find a specific user
+// GET /user/:email - find a specific user using email address as identifier
 
 router.get('/:email', async (req, res, next) => {
   // get just the email out of the uri request
@@ -56,25 +56,53 @@ router.get('/:email', async (req, res, next) => {
   }
 });
 
-// PUT - add info to a user
-router.put('/:username', async (req, res, next) => {
-  // This request comes along with:
-  // 1. username
-  //   and formTwo: firstName, lastName, number
-  // or formThree:
-  const username = req.params.username;
+// POST /user - add/create a new user
+router.post('/', async (req, res, next) => {
+  try {
+    // make sure there is both a username and email
+    if (!req.body.username || !req.body.email) {
+      let error = new Error();
+      error.status = 400;
+      throw error;
+    }
+
+    // add new user to database
+    const addedUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    // send confirmation that the user was added
+    res.status(201).json(addedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT - add info to an existing user
+router.put('/:email', async (req, res, next) => {
+  // This request comes along with email and:
+  // from formTwo: firstName, lastName, number
+  // from formThree: streetAddress, city, state, zipCode
+  console.log('req.body: ', req.body);
+
+  const email = req.params.email;
   try {
     // Does this user exist in the database?
     const user = await User.findOne({
-      where: { username: username },
+      where: { email: email },
     });
+    console.log('found user: ', user);
+    console.log('user id: ', user.dataValues.id);
 
     // if cannot find user in database, send 404
     if (!user) return res.status(404).end();
+    console.log('third see me');
 
     // update the user in the database
     const updatedUser = await user.update(req.body);
-
+    // const updatedUser = await user.update((user.lastName = req.body.lastName));
+    console.log('fourth see me');
     // send confirmation that the user was updated
     res.status(200).json(updatedUser);
   } catch (error) {
