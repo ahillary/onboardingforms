76bd6ff6c99dbc233a4369440180498ef6fce4ca
Hillary Jenning <hello.hillary7@gmail.com>
2022-08-20 10:23:54 -0700

    server(refactor): found user.email only returned
    userRouterFindEmail file solely for finding a user in db using email

--- server/api/userRouterFindEmail.js
+++ server/api/userRouterFindEmail.js
@@ -2,20 +2,18 @@ const router = require('express').Router();
 const User = require('../db/models/user');
 const { connect } = require('../db');
 
-// GET /user/:email - find a specific user using email address as identifier
-
+// GET /:email - find a specific user using email address as identifier
 router.get('/:email', async (req, res, next) => {
   // get just the email out of the uri request
   const email = req.params.email;
-
-  if (!email && !username) {
+  if (!email) {
     res.status(400).end();
     return;
   }
   try {
     // Does this user exist in the database?
     const user = await User.findOne({
-      where: { email: email } || { username: username },
+      where: { email: email },
     });
 
     //  If cannot find user in database, send 404
@@ -24,8 +22,8 @@ router.get('/:email', async (req, res, next) => {
       return;
     }
 
-    // send specific user info
-    res.status(200).json(user);
+    // send specific user's email only
+    res.status(200).json(user.email);
   } catch (error) {
     next(error);
   }
